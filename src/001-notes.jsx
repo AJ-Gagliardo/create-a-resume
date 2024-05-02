aeff963c44cab6bdd04f85bd607a18bcfc3c30e4
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-05-02 11:51:39 -0700

    first commit, just taking a few notes, and organizing myself to create the project

---
+++ src/001-notes.jsx
@@ -0,0 +1,14 @@
+function ThingsToDo() {
+  return (
+    <>
+      List of things to do Identify which components i need Do a simple test on
+      one of those components see which things i can replicate
+    </>
+  );
+}
+
+// General idea would be having on the left the inputs and on the right the output on real time using react and state
+
+// break everyhing up in components
+// maybe can do a general input function , example function Input(){}  and then insert that function in ever other function , example function ComponentOne(){Input()}
+//then <root><Component1><component2> so they can be in order and if user dont insert anything , nothing will appear
